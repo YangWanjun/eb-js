b3b8d337364052f0e70df97353f8cf8eb717063a
YangWanjun <ywjsailor@gmail.com>
2017-11-21 17:19:19 +0900

    Googlemap multi load error

--- src/Map.js
+++ src/Map.js
@@ -1,25 +1,58 @@
+require('ol/ol.css');
+var ol = require('ol').default;
+ol.Map = require('ol/map').default;
+ol.View = require('ol/view').default;
+ol.layer = require('ol/layer/layer').default;
+ol.layer.Vector = require('ol/layer/vector').default;
+ol.source = require('ol/source/source').default;
+ol.proj = require('ol/proj').default;
+KML = require('ol/format/kml').default;
+const loadGoogleMapsAPI = require('load-google-maps-api')
+
 module.exports = Map = function() {
     this.options = null;
     this.gmap = null;
     this.olMap = null;
 };
 
-Map.prototype.init = function(gmap_id, olmap_id, options) {
-    if (this.gmap != null && this.olMap != null) {
+Map.prototype.init = function (gmap_id, olmap_id, options) {
+    if (document.getElementById(gmap_id) == null || document.getElementById(olmap_id) == null) {
         return;
     }
 
+    var self = this;
+    if (window.google) {
+        try {
+            self.InitMap(google.maps, gmap_id, olmap_id);
+        } catch (ex) {
+            console.error(ex);
+        }
+    } else {
+        loadGoogleMapsAPI({ key: 'AIzaSyBjzwjBMykNJikl12HQOuWsYxMmozvkhVU' }).then(function (googleMaps) {
+            console.log(googleMaps) //=> Object { Animation: Object, ... 
+            self.InitMap(googleMaps, gmap_id, olmap_id);
+        }).catch((err) => {
+            console.error(err)
+        });
+    }
+};
+
+Map.prototype.InitMap = function (googleMap, gmap_id, olmap_id) {
     // Base Map [ Google Maps ]
-    var gmap = new google.maps.Map( document.getElementById( 'gmap' ), {
-        mapTypeId: google.maps.MapTypeId.ROADMAP,
+    var gmap = new googleMap.Map( document.getElementById( gmap_id ), {
+        mapTypeId: googleMap.MapTypeId.ROADMAP,
         zoomControl: true,
         scaleControl: true,
         streetViewControl: false,
         fullscreenControl: true,
         fullscreenControlOptions: {
-            position: google.maps.ControlPosition.TOP_RIGHT,
+            position: googleMap.ControlPosition.TOP_RIGHT,
         }
     });
+
+    if (document.getElementById(olmap_id) == null) {
+        return;
+    }
 
     // OL3 view [ Sphere Mercator projection : EPSG:3857 ]
     var view = new ol.View( {
@@ -34,7 +67,7 @@ Map.prototype.init = function(gmap_id, olmap_id, options) {
         var latLng   = ol.proj.transform( smCenter, 'EPSG:3857', 'EPSG:4326' );
 
         // synchronize with Google Maps
-        gmap.setCenter( new google.maps.LatLng( latLng[1], latLng[0] ) );  
+        gmap.setCenter( new googleMap.LatLng( latLng[1], latLng[0] ) );  
     });
 
     // OL3 event handler [ map zoom ]
@@ -44,22 +77,13 @@ Map.prototype.init = function(gmap_id, olmap_id, options) {
     });
 
     // OL3 KML vector object 
-    var vector = new ol.layer.Vector({
-        source: new ol.source.Vector({
-            format: new ol.format.KML()
-        })
-    });
+    var vector = new ol.layer.Vector();
 
     // DOM element for OL3 Vector Layer object
-    var olMapDiv = document.getElementById( 'olmap' );
+    var olMapDiv = document.getElementById( olmap_id );
 
     var olMap = new ol.Map({
         layers: [vector],
-        interactions: ol.interaction.defaults({
-            altShiftDragRotate: false,
-            dragPan: false,
-            rotate: false
-        }).extend([new ol.interaction.DragPan({kinetic: null})]),
         target: olMapDiv,
         controls: [
             //new ol.control.FullScreen()
@@ -74,7 +98,7 @@ Map.prototype.init = function(gmap_id, olmap_id, options) {
 
     //
     olMapDiv.parentNode.removeChild( olMapDiv );
-    gmap.controls[ google.maps.ControlPosition.TOP_RIGHT].push( olMapDiv );
+    gmap.controls[ googleMap.ControlPosition.TOP_RIGHT].push( olMapDiv );
 
     this.gmap = gmap;
     this.olMap = olMap;
